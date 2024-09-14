d43d077237ed10a6770e902a8d63e83333ead43e
hiperbou <hiperbou@gmail.com>
2024-09-15 01:24:44 +0200

    Added imgui-kt library project

---
+++ imgui-kt/build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    kotlin("multiplatform") version "1.7.10"
+    id("convention.publication")
+}
+
+group = "io.github.hiperbou.imgui-kt"
+version = "0.0.1"
+
+repositories {
+    mavenLocal()
+    mavenCentral()
+}
+
+kotlin {
+    js(LEGACY) {
+        browser {
+        }
+    }
+    sourceSets {
+        val jsMain by getting
+
+        jsMain.dependencies {
+            implementation("io.github.hiperbou.imgui-kt:imgui-kt-definitions:0.0.1")
+        }
+    }
+
+}
